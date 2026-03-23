d1362d722b58b669e9da28033262f3ccdbfe1a21
Joshua-Smith-42 <joshua@randomsmiths.com>
2026-03-23 13:41:30 -0400

    initial code written, still needs testing, driver is broken

---
+++ GroupProject.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+from typing import List, Tuple
+from dataclasses import dataclass
+
+import numpy as np
+
+@dataclass
+class Segment:
+    defined_on: Tuple[int, int]
+    """[start, stop) for segment"""
+    curvature: float = 0.0
+    """curvature of the segment"""
+
+class Track:
+    GRAVITY = 9.81
+    
+    def __init__(self, segments: List[Segment] = []):
+        self.path = segments
+
+    def expand_track(self,segments: List[Segment]) -> Track:
+        self.path += segments
+        return self
+
+    def calculate_curvature(self, position: float):
+        for segment in self.path:
+            (start,stop) = segment.defined_on
+            if start <= position < stop:
+                return segment.curvature
+        raise ValueError('position not defined on curve')
+
+class Car:
+    def __init__(self, mass_kg: float, friction_coeffecient:float, track:Track, positions):
+        self.mass = mass_kg
+        self.friction_coeffecient: float = friction_coeffecient
+        self.track = track
+        self.positions = positions
+        velocities = np.gradient(positions)
+        tangential_accels = np.array(np.gradient(velocities))
+        normal_accels = np.array([])
+        for index, position in enumerate(positions):
+            curvature = track.calculate_curvature(position)
+            normal_accel = velocities[index]**2/curvature
+            normal_accels = np.append(normal_accels, normal_accel)
+        self.accelerations = np.sqrt(normal_accels**2 + tangential_accels**2)
+
+    def calculate_downforce(self) -> float:
+        downforces = []
+        gravitational_friction_force = self.track.GRAVITY*self.mass*self.friction_coeffecient
+        downforces = [accel * self.mass - gravitational_friction_force for accel in self.accelerations]
+        return max(downforces)
+    
+race_track = Track([
+    Segment(defined_on=(0,   300),  curvature=80),
+    Segment(defined_on=(300, 600),  curvature=1000),
+    Segment(defined_on=(600, 1000), curvature=50),
+])
+
+positions = np.linspace(1,999,10)
+
+racecar = Car(
+    mass_kg=700,
+    friction_coeffecient=0.6,
+    track=race_track,
+    positions=positions,
+)
+
+downforce = racecar.calculate_downforce()
+print(f"Peak downforce required: {downforce:.1f} N")
